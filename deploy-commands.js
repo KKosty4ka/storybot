b362095dfc32624c4152ae3ba38fe49aeea001d8
KKosty4ka <kostya.dis@outlook.com>
2024-05-20 12:15:10 +0700

    global slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,6 +3,12 @@ const config = require("./config.json");
 const path = require("path");
 const fs = require("fs");
 
+if (process.argv.length !== 3 || (process.argv[2] !== "test" && process.argv[2] !== "global"))
+{
+    console.log("Usage:\nnode deploy-commands.js test   - deploy commands to test guild\nnode deploy-commands.js global - deploy commands globally")
+    process.exit(0);
+}
+
 var commands = [];
 var commandsPath = path.join(__dirname, "commands");
 var commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
@@ -23,14 +29,23 @@ for (var file of commandFiles)
 }
 
 var rest = new REST().setToken(config.token);
+var cmds;
+
+if (process.argv[2] === "test")
+{
+    cmds = Routes.applicationGuildCommands(config.clientId, config.testGuildId);
+}
+else
+{
+    cmds = Routes.applicationCommands(config.clientId);
+}
 
 (async () =>
 {
     console.log(`Reloading ${commands.length} slash commands...`);
 
     await rest.put(
-        // Routes.applicationCommands(config.clientId),
-        Routes.applicationGuildCommands(config.clientId, config.testGuildId),
+        cmds,
         { body: commands }
     );
 
